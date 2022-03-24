a13a0908d890356b8f642e963ab1ce83747dcffe
clozano80 <clozano80@gmail.com>
2022-03-24 10:07:49 +0000

    modal 2 add tab

--- src/components/displaylayout/DisplayLayout.jsx
+++ src/components/displaylayout/DisplayLayout.jsx
@@ -1,194 +1,202 @@
-import React, { useEffect, useState, useCallback } from "react";
-import { FullScreen, useFullScreenHandle } from "react-full-screen";
-import ToggleTheme, { getTheme, setTheme } from "../toggletheme/ToggleTheme";
-import { RiFullscreenFill } from "react-icons/ri";
-import "./displaylayout.css";
+import React, { useEffect, useState, useCallback } from 'react';
+import { FullScreen, useFullScreenHandle } from 'react-full-screen';
+import ToggleTheme, { getTheme, setTheme } from '../toggletheme/ToggleTheme';
+import { RiFullscreenFill } from 'react-icons/ri';
+import './displaylayout.css';
+import MenuLateral from '../menulateral/MenuLateral';
 //Apps
-import Main from "../../apps/main/Main";
-import AppExample from "../../apps/appexample/AppExample";
+import Main from '../../apps/main/Main';
+import AppExample from '../../apps/appexample/AppExample';
 //Layout
-import DropdownMenuTab from "../dropdownmenutab/DropdownMenuTab";
-import { Tabs, Menu, Layout, Drawer } from "antd";
-import { BsMenuButtonWide } from "react-icons/bs";
-import { DatabaseOutlined } from "@ant-design/icons";
+import DropdownMenuTab from '../dropdownmenutab/DropdownMenuTab';
+import { Tabs, Menu, Layout, Drawer } from 'antd';
+import { BsMenuButtonWide } from 'react-icons/bs';
+import { DatabaseOutlined } from '@ant-design/icons';
 const { TabPane } = Tabs;
 const { Header, Content, Footer } = Layout;
 
-let panes = require("../../panes.json");
+let panes = require('../../panes.json');
 
 const Components = {
-  Main: <Main />,
-  AppExample: <AppExample />,
-  DatabaseOutlined: <DatabaseOutlined />,
-  DropdownMenuTab: <DropdownMenuTab />,
+	Main: <Main />,
+	AppExample: <AppExample />,
+	DatabaseOutlined: <DatabaseOutlined />,
+	DropdownMenuTab: <DropdownMenuTab />,
 };
 
 export default function DisplayLayout() {
-  const [state, setState] = useState({
-    activeKey: panes[0].key,
-    panes,
-  });
-  const handle = Array.from({length: panes.length}, (v) => useFullScreenHandle());
+	const [state, setState] = useState({
+		activeKey: panes[0].key,
+		panes,
+	});
 
-  const [visible, setVisible] = useState(false);
-  useEffect(() => setTheme(getTheme()), []);
+	const handle = Array.from({ length: panes.length }, (v) => useFullScreenHandle());
+	const attachFullScreenHandle = () => {
+		try {
+			handle.push(useFullScreenHandle());
+		} catch (err) {
+			console.log(err);
+		}
+	};
+	const [visible, setVisible] = useState(false);
+	useEffect(() => setTheme(getTheme()), []);
 
-  const onChange = (activeKey) => {
-    const { panes } = state;
-    setState({ activeKey: activeKey, panes });
-  };
+	const onChange = (activeKey) => {
+		const { panes } = state;
+		setState({ activeKey: activeKey, panes });
+	};
 
-  const add = () => {
-    //e ? e.preventDefault() : null;
-    const { panes } = state;
-    const addKey = `key_${Date.now()}`;
-    panes.push({
-      title: `Name ${Date.now()}`,
-      content: "New Tab Pane",
-      options: <DropdownMenuTab />,
-      key: addKey,
-      closable: true,
-    });
-    setState({ activeKey: addKey, panes });
-  };
+	const pushToPanes = ({ title, content, options, key, closable }) => {
+		panes.push({
+			title: title,
+			content: content,
+			options: options,
+			key: `key_${Date.now()}`,
+			closable: closable,
+		});
+	};
 
-  const onEdit = (targetKey, action) => {
-    if (action === "remove") {
-      remove(targetKey);
-    } else if (action === "add") {
-      add();
-    }
-  };
+	const add = (pane) => {
+		try {
+			const { panes } = state;
+			const addKey = `key_${Date.now()}`;
+			attachFullScreenHandle();
+			pushToPanes(pane);
+			/*
+			panes.push({
+			  title: `Name ${Date.now()}`,
+			  content: "New Tab Pane",
+			  options: <DropdownMenuTab />,
+			  key: addKey,
+			  closable: true,
+			});
+			*/
+			setState({ activeKey: addKey, panes });
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
-  const remove = (targetKey) => {
-    let { activeKey } = state;
-    const { panes } = state;
-    let lastIndex;
-    panes.forEach((pane, i) => {
-      if (pane.key === targetKey) {
-        lastIndex = i - 1;
-      }
-    });
-    const newPanes = panes.filter((pane) => pane.key !== targetKey);
-    if (panes.length && activeKey === targetKey) {
-      if (lastIndex >= 0) {
-        activeKey = newPanes[lastIndex].key;
-      } else {
-        activeKey = newPanes[0].key;
-      }
-    }
-    setState({ activeKey: activeKey, panes: newPanes });
-  };
-  const menu = (
-    <>
-      <div>
-        Dark/light Theme <ToggleTheme />
-      </div>
-      <Menu>
-        <Menu.Item key="1">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="/"
-            onClick={(e) => {
-              e.preventDefault();
-              add();
-            }}
-          >
-            + Add Tab
-          </a>
-        </Menu.Item>
-      </Menu>
-    </>
-  );
+	const onEdit = (targetKey, action) => {
+		if (action === 'remove') {
+			remove(targetKey);
+		} else if (action === 'add') {
+			add();
+		}
+	};
 
-  const operations = (
-    <div>
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="/"
-        onClick={(e) => {
-          e.preventDefault();
-          setVisible(!visible);
-        }}
-      >
-        <BsMenuButtonWide />
-      </a>
-    </div>
-  );
+	const remove = (targetKey) => {
+		let { activeKey } = state;
+		const { panes } = state;
+		let lastIndex;
+		panes.forEach((pane, i) => {
+			if (pane.key === targetKey) {
+				lastIndex = i - 1;
+			}
+		});
+		const newPanes = panes.filter((pane) => pane.key !== targetKey);
+		if (panes.length && activeKey === targetKey) {
+			if (lastIndex >= 0) {
+				activeKey = newPanes[lastIndex].key;
+			} else {
+				activeKey = newPanes[0].key;
+			}
+		}
+		setState({ activeKey: activeKey, panes: newPanes });
+	};
 
-  const onClose = () => {
-    setVisible(false);
-  };
+	const operations = (
+		<div>
+			<a
+				target="_blank"
+				rel="noopener noreferrer"
+				href="/"
+				onClick={(e) => {
+					e.preventDefault();
+					setVisible(!visible);
+				}}
+			>
+				<BsMenuButtonWide />
+			</a>
+		</div>
+	);
 
-  return (
-    <Layout>
-      <Layout>
-        <Header
-          style={{
-            position: "fixed",
-            zIndex: 1,
-            width: "100%",
-            height: "30px",
-          }}
-        ></Header>
-        <Content
-          className="site-layout"
-          style={{
-            padding: "0",
-            marginTop: 0,
-            height: "calc(100vh - 60px)",
-          }}
-        >
-          <Tabs
-            tabBarExtraContent={operations}
-            hideAdd
-            onChange={onChange}
-            activeKey={state.activeKey}
-            type="editable-card"
-            onEdit={onEdit}
-          >
-            {state.panes.map((pane, i) => {
-              return (
-                <TabPane
-                  tab={
-                    <span>
-                      {Components[pane.icon]}
-                      {pane.title}
-                      {Components[pane.options] || pane.options}
-                    </span>
-                  }
-                  key={pane.key}
-                  closable={pane.closable}
-                >
-                  <div>
-                    <a href="/" target="_blank" rel="noopener noreferrer" className="fixedbutton" onClick={handle[i].enter}>
-                      <RiFullscreenFill />
-                    </a>
-                    <FullScreen handle={handle[i]}>
-                      {Components[pane.content] || pane.content}
-                    </FullScreen>
-                  </div>
-                </TabPane>
-              );
-            })}
-            ;
-          </Tabs>
-        </Content>
-        <Footer>
-          <span>Footer</span>
-        </Footer>
-      </Layout>
-      <Drawer
-        title="Options"
-        placement="right"
-        width="30vw"
-        onClose={onClose}
-        visible={visible}
-      >
-        {menu}
-      </Drawer>
-    </Layout>
-  );
+	const onClose = () => {
+		setVisible(false);
+	};
+
+	return (
+		<Layout>
+			<Layout>
+				<Header
+					style={{
+						position: 'fixed',
+						zIndex: 1,
+						width: '100%',
+						height: '30px',
+					}}
+				></Header>
+				<Content
+					className="site-layout"
+					style={{
+						padding: '0',
+						marginTop: 0,
+						height: 'calc(100vh - 60px)',
+					}}
+				>
+					<Tabs
+						tabBarExtraContent={operations}
+						hideAdd
+						onChange={onChange}
+						activeKey={state.activeKey}
+						type="editable-card"
+						onEdit={onEdit}
+					>
+						{state.panes.map((pane, i) => {
+							return (
+								<TabPane
+									tab={
+										<span>
+											{Components[pane.icon]}
+											{pane.title}
+											{Components[pane.options] || pane.options}
+										</span>
+									}
+									key={pane.key}
+									closable={pane.closable}
+								>
+									<div>
+										<a
+											href="/"
+											target="_blank"
+											rel="noopener noreferrer"
+											className="fixedbutton"
+											onClick={handle[i].enter}
+										>
+											<RiFullscreenFill />
+										</a>
+										<FullScreen handle={handle[i]}>
+											{Components[pane.content] || pane.content}
+										</FullScreen>
+									</div>
+								</TabPane>
+							);
+						})}
+					</Tabs>
+				</Content>
+				<Footer>
+					<span>Footer</span>
+				</Footer>
+			</Layout>
+			<Drawer
+				title="Options"
+				placement="right"
+				width="30vw"
+				onClose={onClose}
+				visible={visible}
+			>
+				<MenuLateral add={add} panes={panes} setVisible={setVisible} />
+			</Drawer>
+		</Layout>
+	);
 }
---
+++ src/components/menulateral/MenuLateral.jsx
@@ -0,0 +1,57 @@
+import ToggleTheme, { getTheme, setTheme } from '../toggletheme/ToggleTheme';
+import { useState } from 'react';
+import { Menu, Modal, Button } from 'antd';
+import { FullScreen, useFullScreenHandle } from 'react-full-screen';
+export default function MenuLateral({ add, panes, setVisible }) {
+	const [isModalVisible, setIsModalVisible] = useState(false);
+
+	const showModal = () => {
+		setIsModalVisible(true);
+	};
+
+	const handleCancel = () => {
+		setIsModalVisible(false);
+	};
+	return (
+		<>
+			<div>
+				Dark/light Theme <ToggleTheme />
+			</div>
+			<Menu>
+				<Menu.Item key="1">
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="/"
+						onClick={(e) => {
+							e.preventDefault();
+							setVisible(false);
+							showModal();
+						}}
+					>
+						+ Add Tab
+					</a>
+				</Menu.Item>
+			</Menu>
+			<Modal title="Seleccion" visible={isModalVisible} footer={null} onCancel={handleCancel}>
+				<Menu>
+					{panes.map((pane, i) => (
+						<Menu.Item key={i}>					
+							<a
+								target="_blank"
+								rel="noopener noreferrer"
+								href="/"
+								onClick={(e) => {
+									e.preventDefault();
+									add(pane);
+								}}
+							>
+								+ {pane.title}
+							</a>
+						</Menu.Item>
+					))}
+				</Menu>
+			</Modal>
+		</>
+	);
+}
